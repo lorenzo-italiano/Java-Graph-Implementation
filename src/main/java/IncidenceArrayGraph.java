1dee8c1172d9cf7fe7f9387b4550be70ce35aa60
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2022-05-08 14:49:40 +0200

    IncidenceArrayGraph skeleton.

---
+++ src/main/java/IncidenceArrayGraph.java
@@ -0,0 +1,51 @@
+public class IncidenceArrayGraph implements Graph {
+    @Override
+    public int nbOfVertices() {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public int nbOfEdges() {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public void addVertex(Vertex vertex) {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public void addEdge(Vertex vertex1, Vertex vertex2, Edge edge) {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public boolean isConnected(Vertex vertex1, Vertex vertex2) {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public boolean isConnected() {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public Edge[] getEdges(Vertex vertex1, Vertex vertex2) {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public Edge[] getEdges() {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public Vertex[] getVertices() {
+        throw new RuntimeException("Not implemented. ");
+    }
+
+    @Override
+    public Edge[] getNeighborEdges(Vertex vertex) {
+        throw new RuntimeException("Not implemented. ");
+    }
+}
